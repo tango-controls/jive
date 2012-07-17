48a3bf35cbf4bbd461a1f8314a514376ab182781
jlpons <JeanLucPons@users.noreply.github.com>
2012-07-17 14:36:29 +0000

    Changed release number

--- jive3/MainPanel.java
+++ jive3/MainPanel.java
@@ -48,7 +48,7 @@ public class MainPanel extends JFrame implements ChangeListener {
   private boolean running_from_shell;
 
   // Relase number (Let a space after the release number)
-  final static private String appVersion = "Jive 4.24 ";
+  final static private String appVersion = "Jive 4.25 ";
 
   // General constructor
   public MainPanel() {
