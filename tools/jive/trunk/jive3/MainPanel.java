a14af2527584d4f3614201dc80d3d6ee36ff05ad
jlpons <jlpons@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2013-12-05 16:01:32 +0000

    Added unicode parsing in parse_string()

--- tools/jive/trunk/jive3/MainPanel.java
+++ tools/jive/trunk/jive3/MainPanel.java
@@ -50,7 +50,7 @@ public class MainPanel extends JFrame implements ChangeListener {
   private boolean running_from_shell;
 
   // Relase number (Let a space after the release number)
-  final static private String appVersion = "Jive 4.33 ";
+  final static private String appVersion = "Jive 4.34 ";
 
   // General constructor
   public MainPanel() {
